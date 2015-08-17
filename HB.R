4597261ad6d84e1397be5a4e954f05ef424423f1
tc-mustang <rjl278@cornell.edu>
2015-08-17 14:19:00 -0400

    Graphs HB using ggplot2

--- HB.R
+++ HB.R
@@ -1,25 +1,5 @@
 library(ggplot2)
 
-for(i in 1:2) {
-  infile <- paste("/home/DB/M.esculenta/VCF_V6/HB/block",i,".blocks.det", sep="")
-  
-  block <- read.csv(infile, sep="")
-  
-  y <- block[which(block[,4]>1),4]
-  x <- block[which(block[,4]>1),2]
-  z <- rep(i, length(y))
-  
-  data <- as.data.frame(x)
-  data[,2] <- y
-  data[,3] <- z
-  colnames(data) <- c("position", "size", "chrom")
-  
-  a <- ggplot(data, aes(x=position, y=size, color=chrom)) + geom_point(shape=9)
-  a
-  
-}
-
-
 ################################################################################################################################################
 
 all <- read.table("/home/DB/M.esculenta/VCF_V6/HB/all.blocks", header=TRUE, quote="\"")
@@ -30,13 +10,23 @@ y <- y[-(which(y[,1] == "CHR")),]
 size <- y[,4]
 num <- as.numeric(levels(size))[size]
 cho <- which(num>1)
+
 y <- y[cho,]
+
+
 
 #how much of the genome does the haplotype blocks cover
 
 sum(as.numeric(levels(y[,4]))[y[,4]])
+sum(as.numeric(levels(y[,5]))[y[,5]])
 
+#histogram of haplotypes bigger than 50kb
 
+a <- which(as.numeric(levels(y[,4]))[y[,4]]> 100)
+b <-(y[a,4])
+hist(as.numeric(levels(b))[b], xlab = "size (kb)", main ="HB size Distribution (higher than 100kb)", col="orange")
+
+hist(as.numeric(levels(y[,4]))[y[,4]], xlab = "size (kb)", main ="HB size Distribution", col= "orange")
 
 #GRAPH ALL
 
@@ -57,5 +47,30 @@ p + geom_point(color="chartreuse4") +  ylim(0, 200) +
   xlab("Position in chromosomes") +
   ylab("HB size in Kb") +
   labs(color = "CHR")
-                       
-                       
+
+#extract a single chromsome
+
+setwd("/home/DB/M.esculenta/VCF_V6/HB/")
+
+for (i in 1:20){  
+
+  num <- i
+  selection <- which(y$CHR==num)
+  subset <- y[selection,]
+
+  g <- ggplot(subset, aes(x=as.numeric(levels(BP1))[BP1], y=as.numeric(levels(KB))[KB])) 
+  
+  chr <- g + geom_point(aes(size = as.numeric(levels(NSNPS))[NSNPS]), color= "chartreuse4")  +
+      #scale_colour_gradient(low = "blue", limits = c(10,80)) +
+      labs(title = paste("Haplotype Blocks Chromosome ",num)) +
+      theme(plot.title = element_text(size=20, face="bold", vjust=2)) +
+      xlab("Position in chromosomes") +
+      ylab("HB size in Kb") +
+      labs(size = "NSNPS")
+  
+  namef <- paste("chr",num,".pdf")
+  ggsave(chr, file=namef)
+ 
+                         
+}    
+
